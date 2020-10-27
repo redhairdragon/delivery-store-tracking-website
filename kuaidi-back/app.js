0d69f98654c4dc22f2271bf45043e76027fea105
Cocoa Starrion <shen_teng@engineering.ucla.edu>
2020-10-27 02:00:10 -0700

    Deployment related changes

--- kuaidi-back/app.js
+++ kuaidi-back/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
 
 var adminRouter = require('./routes/admin');
 var customerRouter = require('./routes/customer');
@@ -15,22 +15,42 @@ var mysqlConn = mysql.createConnection({
     password: 'woaiKuaidi!23',
     database: 'PostOffice'
 });
+
+function handleDisconnect(conn) {
+    conn.on('error', function (err) {
+        if (!err.fatal) {
+            return;
+        }
+
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+
+        console.log('Re-connecting lost connection: ' + err.stack);
+
+        mysqlConn = mysql.createConnection(conn.config);
+        handleDisconnect(mysqlConn);
+        mysqlConn.connect();
+        app.set('mysqlConn', mysqlConn);
+    });
+}
+handleDisconnect(mysqlConn);
 mysqlConn.connect();
 
 
 var app = express();
 app.set('mysqlConn', mysqlConn);
-app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))
+// app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'kuaidi-front')));
 
 
 // app.use('/', indexRouter);
@@ -38,12 +58,12 @@ app.use('/admin', adminRouter);
 app.use('/customer/', customerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,6 +71,11 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+});
+
+app.all('/*', function (req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('kuaidi-front/index.html', { root: __dirname });
 });
 
 module.exports = app;
